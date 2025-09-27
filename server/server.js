eda7841fa4a54d384dd24bf01f28afd807fe4db1
rizwanmoulvi <rizwan.m.1610@gmail.com>
2025-09-28 01:07:22 +0530

    fixed the cors

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ const app = express();
 
 // CORS configuration - allow all origins for development
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: "https://upal.rizzmo.site",  // not "*"
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
 }));
 
 app.use(express.json());
